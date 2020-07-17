dd9a858f3c23b161f21ef331196ea935a5a8748f
Saswat <saswatsubhajyotimallick@gmail.com>
2020-07-17 18:04:38 +0530

    updates

--- core/roi_layers/ps_roi_align.py
+++ core/roi_layers/ps_roi_align.py
@@ -43,5 +43,25 @@ def ps_roi_align(input, boxes, output_size, spatial_scale=1.0, sampling_ratio=-1
     return output
 
 
+class PSRoIAlign(nn.Module):
+    """
+    See ps_roi_align
+    """
+    def __init__(self, output_size, spatial_scale, sampling_ratio):
+        super(PSRoIAlign, self).__init__()
+        self.output_size = output_size
+        self.spatial_scale = spatial_scale
+        self.sampling_ratio = sampling_ratio
 
+    def forward(self, input, rois):
+        return ps_roi_align(input, rois, self.output_size, self.spatial_scale,
+                            self.sampling_ratio)
+
+    def __repr__(self):
+        tmpstr = self.__class__.__name__ + '('
+        tmpstr += 'output_size=' + str(self.output_size)
+        tmpstr += ', spatial_scale=' + str(self.spatial_scale)
+        tmpstr += ', sampling_ratio=' + str(self.sampling_ratio)
+        tmpstr += ')'
+        return tmpstr
         
